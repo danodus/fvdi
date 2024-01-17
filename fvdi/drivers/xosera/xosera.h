c110eb085048700652501bddbaf73a28f6eb717e
Daniel Cliche <danclichegmail.com>
2024-01-17 09:05:46 -0500

    Fix Xosera playfield not unblanked

--- fvdi/drivers/xosera/xosera.h
+++ fvdi/drivers/xosera/xosera.h
@@ -1,7 +1,7 @@
 #ifndef INCLUDE_XOSERA_H
 #define INCLUDE_XOSERA_H
 
-#define ROSCO_M68K          // needed for rosco_m68k Xosera HW defines
+#define ROSCO_M68K  // needed for rosco_m68k Xosera HW defines
 
 #include "util.h"
 // #include <machine.h>

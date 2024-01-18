41d65675877c4e7dff63998a6457234643a5210d
Xark <Xark@XarkLabs.com>
2024-01-17 23:01:01 -0800

    remove Xosera API and use $(XOSERA_M68K_API) env var

--- fvdi/drivers/xosera/xosera.h
+++ fvdi/drivers/xosera/xosera.h
@@ -1,10 +1,10 @@
 #ifndef INCLUDE_XOSERA_H
 #define INCLUDE_XOSERA_H
 
-#define ROSCO_M68K  // needed for rosco_m68k Xosera HW defines
+#define ROSCO_M68K          // needed for rosco_m68k Xosera HW defines
 
 #include "util.h"
-//#include <machine.h>
+// #include <machine.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
